25735f49efbe30ee2b25c408a46a3d6d3af12d78
daanh432 <daan@daanhendriks.nl>
2020-10-01 12:58:08 +0200

    Implement queue command placeholder

---
+++ src/main/java/nl/daanh/hiromi/commands/music/QueueCommand.java
@@ -0,0 +1,11 @@
+package nl.daanh.hiromi.commands.music;
+
+import nl.daanh.hiromi.commands.context.CommandContext;
+import nl.daanh.hiromi.commands.context.CommandInterface;
+
+public class QueueCommand implements CommandInterface {
+    @Override
+    public void handle(CommandContext ctx) {
+        // TODO Implement queue command
+    }
+}
