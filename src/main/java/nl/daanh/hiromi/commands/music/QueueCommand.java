2fcb6b59a2d9b898071cad0d8a9ca8cfaccd52e3
daanh432 <daan@daanhendriks.nl>
2020-10-01 13:06:36 +0200

    Added command parameters to queue command

--- src/main/java/nl/daanh/hiromi/commands/music/QueueCommand.java
+++ src/main/java/nl/daanh/hiromi/commands/music/QueueCommand.java
@@ -1,8 +1,22 @@
 package nl.daanh.hiromi.commands.music;
 
+import net.dv8tion.jda.api.Permission;
+import nl.daanh.hiromi.commands.annotations.CommandCategory;
+import nl.daanh.hiromi.commands.annotations.CommandHelp;
+import nl.daanh.hiromi.commands.annotations.CommandInvoke;
+import nl.daanh.hiromi.commands.annotations.SelfPermission;
 import nl.daanh.hiromi.commands.context.CommandContext;
 import nl.daanh.hiromi.commands.context.CommandInterface;
 
+@CommandInvoke("queue")
+@CommandInvoke("queued")
+@CommandHelp("Shows you a list of the currently queued songs.")
+@CommandCategory(CommandCategory.CATEGORY.MUSIC)
+@SelfPermission(Permission.MESSAGE_WRITE)
+@SelfPermission(Permission.MESSAGE_ADD_REACTION)
+@SelfPermission(Permission.MESSAGE_MANAGE)
+@SelfPermission(Permission.VOICE_CONNECT)
+@SelfPermission(Permission.VOICE_SPEAK)
 public class QueueCommand implements CommandInterface {
     @Override
     public void handle(CommandContext ctx) {
