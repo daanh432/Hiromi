69c85c4411689a4ade6371b4d2e138548b29e606
daanh432 <daan@daanhendriks.nl>
2020-10-04 14:39:31 +0200

    On track stuck next song

--- src/main/java/nl/daanh/hiromi/music/TrackScheduler.java
+++ src/main/java/nl/daanh/hiromi/music/TrackScheduler.java
@@ -114,13 +114,14 @@ public class TrackScheduler extends AudioEventAdapter {
     @Override
     public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception) {
         // An already playing track threw an exception (track end event will still be received separately)
-        this.guildMusicManager.getLastChannel().sendMessage("Something went wrong in audio playback").queue();
+        this.guildMusicManager.getLastChannel().sendMessage("Something went wrong in audio playback.").queue();
     }
 
     @Override
     public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
         // Audio track has been unable to provide us any audio, might want to just start a new track
-        this.guildMusicManager.getLastChannel().sendMessage("Something went wrong in audio playback").queue();
+        this.guildMusicManager.getLastChannel().sendMessage("Looks like the track got stuck. Lets try to go to the next song.").queue();
+        this.nextTrack();
     }
 }
 
