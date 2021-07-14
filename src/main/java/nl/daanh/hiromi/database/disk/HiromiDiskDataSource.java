1dccd2630ff67dc94775d3aa8b27865aed4e2a20
daanh432 <daan@daanhendriks.nl>
2021-07-14 16:02:31 +0200

    Implemented user settings in the disk data source

--- src/main/java/nl/daanh/hiromi/database/disk/HiromiDiskDataSource.java
+++ src/main/java/nl/daanh/hiromi/database/disk/HiromiDiskDataSource.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import nl.daanh.hiromi.database.IDatabaseManager;
 import nl.daanh.hiromi.exceptions.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,35 +20,64 @@ import java.util.Properties;
 public class HiromiDiskDataSource implements IDatabaseManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(HiromiDiskDataSource.class);
     private static final int EXPIRES_IN = 60; // 60 seconds
-    private final HashMap<Long, Properties> settingsCache = new HashMap<>();
+    private final HashMap<Long, Properties> guildCache = new HashMap<>();
+    private final HashMap<Long, Properties> userCache = new HashMap<>();
 
-    private File getGuildFile(Guild guild) {
+    private File getFile(Guild guild) {
         return new File("./guildConfigs/" + guild.getIdLong() + "/config.properties");
     }
 
-    private Properties fetchSettings(Guild guild) {
-        if (this.settingsCache.containsKey(guild.getIdLong())) {
-            Properties setting = this.settingsCache.get(guild.getIdLong());
+    private File getFile(User user) {
+        return new File("./userConfigs/" + user.getIdLong() + "/config.properties");
+    }
+
+    private void writeKey(String key, String value, Properties properties, File settingsFile, String name, long idLong) {
+        try {
+            properties.setProperty(key, value);
+
+            if (!settingsFile.exists())
+                if (settingsFile.getParentFile().mkdirs())
+                    LOGGER.debug(String.format("Created the initial file structure for the guild: %s", name));
+
+            FileWriter writer = new FileWriter(settingsFile);
+            properties.store(writer, String.format("ID: %s", idLong));
+            writer.close();
+        } catch (IOException e) {
+            throw new HiromiDiskIOException(e);
+        }
+    }
+
+    @NotNull
+    private Properties fetchSettings(long idLong, File file, HashMap<Long, Properties> cache) {
+        if (cache.containsKey(idLong)) {
+            Properties setting = cache.get(idLong);
             if (Long.parseLong(setting.getProperty("local_expires_at", "0")) > Instant.now().getEpochSecond()) {
                 return setting;
             }
         }
 
-        File guildFile = this.getGuildFile(guild);
-        if (!guildFile.exists()) return new Properties();
+        if (!file.exists()) return new Properties();
 
         try {
-            FileReader reader = new FileReader(guildFile);
+            FileReader reader = new FileReader(file);
             Properties properties = new Properties();
             properties.load(reader);
             reader.close();
 
             properties.put("local_expires_at", String.valueOf(Instant.now().getEpochSecond() + EXPIRES_IN));
-            this.settingsCache.put(guild.getIdLong(), properties);
+            cache.put(idLong, properties);
             return properties;
         } catch (IOException e) {
             throw new HiromiDiskIOException(e);
         }
+    }
+
+    private Properties fetchSettings(Guild guild) {
+        return fetchSettings(guild.getIdLong(), this.getFile(guild), guildCache);
+    }
+
+    private Properties fetchSettings(User user) {
+        return fetchSettings(user.getIdLong(), this.getFile(user), userCache);
     }
 
     @Override
@@ -62,21 +92,22 @@ public class HiromiDiskDataSource implements IDatabaseManager {
 
     @Override
     public void writeKey(Guild guild, String key, String value) {
-        try {
-            Properties properties = this.fetchSettings(guild);
-            properties.setProperty(key, value);
+        writeKey(key, value, this.fetchSettings(guild), this.getFile(guild), guild.getName(), guild.getIdLong());
+    }
 
-            File settingsFile = this.getGuildFile(guild);
-            if (!settingsFile.exists())
-                if (settingsFile.getParentFile().mkdirs())
-                    LOGGER.debug(String.format("Created the initial file structure for the guild: %s", guild.getName()));
-
-            FileWriter writer = new FileWriter(settingsFile);
-            properties.store(writer, String.format("Guild: %s", guild.getIdLong()));
-            writer.close();
-        } catch (IOException e) {
-            throw new HiromiDiskIOException(e);
+    @Override
+    public String getKey(User user, String key) {
+        Properties setting = this.fetchSettings(user);
+        if (setting.containsKey(key)) {
+            return setting.getProperty(key);
         }
+
+        return this.getDefaultSetting(key);
+    }
+
+    @Override
+    public void writeKey(User user, String key, String value) {
+        writeKey(key, value, this.fetchSettings(user), this.getFile(user), user.getName(), user.getIdLong());
     }
 
     @Override
@@ -85,17 +116,7 @@ public class HiromiDiskDataSource implements IDatabaseManager {
     }
 
     @Override
-    public String getKey(User user, String key) {
-        throw new NotImplementedException();
-    }
-
-    @Override
     public void writeKey(Member member, String key, String value) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void writeKey(User user, String key, String value) {
         throw new NotImplementedException();
     }
 }
